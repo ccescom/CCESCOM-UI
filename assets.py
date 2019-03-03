8475e2ac3c7468e5c61baca5c17594d8eeddbedd
RanjanBalapppa <ranjan.balappa@gmail.com>
2019-03-03 12:35:21 +0530

    Assests py

--- assets.py
+++ assets.py
@@ -7,17 +7,23 @@ from . import app, webassets
 js_all = Bundle("node_modules/jquery/dist/jquery.min.js",
                 "js/popper.min.js",
                  "node_modules/bootstrap/dist/js/bootstrap.min.js",
+                 "js/paho-mqtt.js",
+                "js/mqtt.min.js",
                  "js/d3.v4.min.js",
+                 "js/d3-simple-slider.js",
+                 "js/d3-tip.js",
                  "js/allocation.js",
-                 "js/materialize.min.js",
+                 "js/simulation.js",
+                "js/crop-hours.js",
                 "js/cc.js",
                  filters="jsmin",
                  output="js/libs.js")
 
 
 css_all = Bundle(
+                  
                   Bundle("node_modules/bootstrap/dist/css/bootstrap.css"),
-                  Bundle("css/materialize.min.css"),
+                  Bundle("css/font-awesome.min.css"),
                   Bundle("css/cc.css"),
                   filters="cssmin",
                   output="css/main.css")
